03e30abece8880d277560a2f4b1bb6df59bac8da
Andrea Zapata <95andreazapata@gmail.com>
2023-03-10 16:59:47 -0500

    Display Balance, Income & Expense

--- script.js
+++ script.js
@@ -37,12 +37,24 @@ function addTransactionDOM(transaction) {
 // Update the balance income and expense
 
 function updateValues() {
-    const amounts = transactions.map(transaction => transaction.amount);
-    
-    const total = amounts.reduce((acc, item) => (acc += item), 0)
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
 
-    console.log(total);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText= `$${income}`;
+  money_minus.innerText= `$${expense}`;
+  
 }
 
 // Init app
@@ -55,5 +67,3 @@ function init() {
 }
 
 init();
-
-
